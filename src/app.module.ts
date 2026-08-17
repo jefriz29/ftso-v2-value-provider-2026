7c85b16605dc1b8740a65712c5035ec18aec761f
Jefferson Calvo <57133058+jefriz29@users.noreply.github.com>
2026-08-17 14:44:17 +1000

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { RandomFeed } from './data-feeds/random-feed';
 import { BaseDataFeed } from './data-feeds/base-feed';
 import { FixedFeed } from './data-feeds/fixed-feed';
 import { FtsoFeedV1 } from './data-feeds/ftso-feed-v1';
+import { FtsoFeedWebSocket } from './data-feeds/ftso-feed-websocket';
 
 @Module({
   imports: [],
@@ -16,22 +17,25 @@ import { FtsoFeedV1 } from './data-feeds/ftso-feed-v1';
       useFactory: async () => {
         let dataFeed: BaseDataFeed;
 
-        if (process.env.VALUE_PROVIDER_IMPL == 'fixed') {
+        if (process.env.VALUE_PROVIDER_IMPL === 'fixed') {
           dataFeed = new FixedFeed();
-        } else if (process.env.VALUE_PROVIDER_IMPL == 'random') {
+        } else if (process.env.VALUE_PROVIDER_IMPL === 'random') {
           dataFeed = new RandomFeed();
-        } else if (process.env.VALUE_PROVIDER_IMPL == 'ftso-v1') {
+        } else if (process.env.VALUE_PROVIDER_IMPL === 'ftso-v1') {
           const ftsoFeed = new FtsoFeedV1();
           await ftsoFeed.start();
           dataFeed = ftsoFeed;
+        } else if (process.env.VALUE_PROVIDER_IMPL === 'ftso-websocket') {
+          const websocketFeed = new FtsoFeedWebSocket();
+          await websocketFeed.start();
+          dataFeed = websocketFeed;
         } else {
           const ccxtFeed = new CcxtFeed();
           await ccxtFeed.start();
           dataFeed = ccxtFeed;
         }
 
-        const service = new ExampleProviderService(dataFeed);
-        return service;
+        return new ExampleProviderService(dataFeed);
       },
     },
   ],
